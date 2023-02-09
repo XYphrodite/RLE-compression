109a9a7e2c0ddd142fb561148eae0797e7811b35
Maksim <mrmaksoracle89537089833@yandex.ru>
2023-02-09 21:14:33 +0300

    Добавьте файлы проекта.

---
+++ Crypt2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Crypt2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.открытьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сжать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Расжать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(8, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 297);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьФайлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(339, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // открытьФайлToolStripMenuItem
+            // 
+            this.открытьФайлToolStripMenuItem.Name = "открытьФайлToolStripMenuItem";
+            this.открытьФайлToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.открытьФайлToolStripMenuItem.Text = "Открыть файл";
+            this.открытьФайлToolStripMenuItem.Click += new System.EventHandler(this.открытьФайлToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 363);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem открытьФайлToolStripMenuItem;
+    }
+}
---
+++ Crypt2/Form1.cs
@@ -0,0 +1,153 @@
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Crypt2
+{
+    public partial class Form1 : Form
+    {
+        private string fileContent = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "txt files (*.txt)|*.txt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileStream = dialog.OpenFile();
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    fileContent = reader.ReadToEnd();
+                }
+                if (fileContent.Count() == 10000)
+                    setPicture();
+                else
+                {
+                    MessageBox.Show($"Неправильная длина строки {fileContent.Count()}", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка чтения файла", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+        }
+
+        private void setPicture()
+        {
+            var bitmap = new Bitmap(100, 100);
+            for (int x = 0; x < 100; x++)
+            {
+                for (int y = 0; y < 100; y++)
+                {
+                    if (fileContent[x * 100 + y] == '1')
+                        bitmap.SetPixel(x, y, Color.Black);
+                    else if (fileContent[x * 100 + y] == '0')
+                        bitmap.SetPixel(x, y, Color.White);
+                    else
+                        MessageBox.Show("Обнаружен символ отличный от 0 и 1", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            pictureBox1.Image = bitmap;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string compressedFile = compresseFile();
+            File.WriteAllText("2.txt", compressedFile);
+        }
+
+        private string compresseFile()
+        {
+            string toReturn = "";
+            int numberOfMatches = 0;
+            for (int i = 0; i < 10000 / 8;i++)
+            {
+                toReturn += "11";
+                int amount = 0;
+                string toCode = fileContent.Substring(i * 8, 8);
+                while (amount <= 63 && i+1 < 10000 / 8)
+                {
+                    string nextCode = fileContent.Substring((i + 1) * 8, 8);
+                    if (toCode == nextCode)
+                    {
+                        i += 1;
+                        amount++;
+                        
+                    }
+                    else
+                    {
+                        numberOfMatches+=amount;
+                        break;
+                    }
+                }
+                string binary = SetZeroes(Convert.ToString(amount, 2));
+                toReturn += binary;
+                toReturn += toCode;
+            }
+            MessageBox.Show($"Количество совпадений = {numberOfMatches}");
+            return toReturn;
+        }
+        private string SetZeroes(string value)
+        {
+            string newStr = "";
+            for (int i = 0; i < 6 - value.Length; i++)
+            {
+                newStr += "0";
+            }
+            newStr += value;
+            return newStr;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string compressedFile = string.Empty;
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "txt files (*.txt)|*.txt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileStream = dialog.OpenFile();
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    compressedFile = reader.ReadToEnd();
+                }
+                for (int x = 0; x < 100; x++)
+                    for (int y = 0; y < 100; y++)
+                        if (fileContent[x * 100 + y] != '1' && fileContent[x * 100 + y] != '0')
+                        {
+                            MessageBox.Show("Обнаружен символ отличный от 0 и 1", "Ошибка",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                string decompressFile = decompresseFile(compressedFile);
+                File.WriteAllText("3.txt", decompressFile);
+            }
+            else
+            {
+                MessageBox.Show("Ошибка чтения файла", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private string decompresseFile(string file)
+        {
+            string toReturn = string.Empty;
+            int numberOfMatches = 0;
+            //decompress algorithm
+
+            MessageBox.Show($"Количество совпадений = {numberOfMatches}");
+            return toReturn;
+        }
+    }
+}
